9f308d6eba7707ec93e56605274c49e2601f7e9d
danipetro21 <idany346@gmail.com>
2023-06-09 14:57:47 +0200

    add bonus

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,6 +1,7 @@
 package org.lessons.java.christmas;
 
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
@@ -8,12 +9,15 @@ import java.util.Collections;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner scan = new Scanner(System.in);
         ArrayList<String> listaRegali = new ArrayList<>();
         boolean exit = false;
         String scelta;
+
+        readFile("wishList.txt");
+
         System.out.println("Crea una lista di regali!");
 
         String regaloIn;
@@ -28,17 +32,50 @@ public class Main {
             System.out.print("Vuoi continuare ad aggiungere regali? s/n:  ");
             scelta = scan.nextLine();
 
-            if(scelta.equals("n")){
+            if (scelta.equals("n")) {
                 exit = true;
             }
 
-        }while(!exit);
+        } while (!exit);
 
 
         Collections.sort(listaRegali);
         System.out.println(listaRegali);
 
 
+        //scrittura su file
+        scritturaSuFile(listaRegali);
+
+    }
+
+
+    public static void scritturaSuFile(ArrayList listaRegali) {
+        try {
+            File file = new File("wishList.txt");
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            for (int i = 0; i < listaRegali.size(); i++) {
+                bufferedWriter.write("Regalo: " + (i + 1) + " " + listaRegali.get(i) + "\n");
+            }
+
+            bufferedWriter.close();
+            System.out.println("Scrittura completata.");
+        } catch (IOException e) {
+            System.out.println("Si è verificato un errore durante la scrittura del file.");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void readFile(String nameList) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(nameList));
+        String line;
+        System.out.println("I regali della vecchia lista sono:");
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line );
+        }
 
     }
 }
